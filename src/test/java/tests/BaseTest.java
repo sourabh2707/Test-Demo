2efd301f3155e6f493bfbe44978e13259fa90612
sourabh2707 <107661536+sourabh2707@users.noreply.github.com>
2022-11-02 15:17:20 +0530

    Update BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -14,6 +14,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	//commit 1
+	//Please add new commit
 	public static WebDriver driver;
 	public Properties prop;
 	FileInputStream fis;
